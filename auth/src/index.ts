8b6d80e305605195183690771a0aba033c31a235
Sukhbir Singh <sukhbirbrar83@gmail.com>
2023-08-20 15:40:45 -0700

    Creted a common code sharing Library using npm && updated imports

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,17 +6,20 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log('Connected to MongoDb');
+    console.log('Connected to Auth MongoDb');
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+    console.log('Listening Auth Service on port 3000');
   });
 };
 
